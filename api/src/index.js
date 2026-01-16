a9bc9f739fd95a555f244a0fedec5fa8312d2131
Rizeth <rcmarcelo@fit.edu.ph>
2026-01-16 13:01:49 +0000

    rewrite and index edited

--- api/src/index.js
+++ api/src/index.js
@@ -1,9 +1,1 @@
-import { app } from "@azure/functions";
-
-// Optional: Global setup like streaming
-app.setup({
-    enableHttpStream: true,
-});
-
-// You don't necessarily need to import rewrite.js here 
-// IF your package.json "main" points to "src/functions/*.js"
+import "./functions/rewrite.js";
